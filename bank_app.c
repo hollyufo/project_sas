f3d8b7c47fa47978653cb4b7ba7668b19cd0f2d9
holly ufo <chaibi1imran@gmail.com>
2021-11-21 15:59:52 +0100

    insert function

--- bank_app.c
+++ bank_app.c
@@ -1,7 +1,23 @@
 #include<stdio.h>
 struct costumer
 {
- int rollno;
+ char ID[20];
  char name[30];
- float mark;
+ int accountnum;
+ float balance;
 }user;
+// function to insert records to the file
+void insert(){
+  FILE *fp;
+  fp = fopen("Record", "a");
+  printf("Enter Id number : ");
+  scanf("%s", &user.ID);
+  printf("Enter the Name : ");
+  scanf("%s", &user.name);
+  printf("Enter account number : ");
+  scanf("%d", &user.accountnum)
+  printf("Enter the balance : ");
+  scanf("%f", &user.balance);
+  fwrite(user, sizeof(user), 1, fp);
+  fclose(fp);
+}
