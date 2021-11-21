37522cb1c983d5cc36846e5213742bb063d933d9
holly ufo <chaibi1imran@gmail.com>
2021-11-21 16:16:53 +0100

    search function

--- bank_app.c
+++ bank_app.c
@@ -21,3 +21,40 @@ void insert(){
   fwrite(user, sizeof(user), 1, fp);
   fclose(fp);
 }
+//    FUNCTION TO DISPLAY RECORDS
+void disp()
+{
+    FILE *fp1;
+    fp1 = fopen("Record", "r");
+    printf("\nid Number\tName\tbalance\tbalance\n\n");
+    while (fread(&stud, sizeof(stud), 1, fp1))
+    printf("  %s\t\t%s\t%d\t%.2f\n", student.id, user.name, user.accountnum, user.balance);
+    fclose(fp1);
+}
+// function to search 
+void search(){
+    FILE *fp2;
+    int r, s, t;
+    printf("\nEnter the id you want to search : ");
+    scanf("%s", &r);
+    t = tid(r);
+    if (t == 0){
+        printf("the id %s is not available in the records.\n", r);
+    }
+    else {
+        fp2 = fopen("Record", "r");
+        while (fread(&user, sizeof(user), 1, fp2))
+        {
+            s = user.ID;
+            if (s == r){
+                printf("\nid  = %s", user.ID);
+                printf("\nName    = %s", user.name);
+                printf("\nbalnace    = %.2f\n", user.balance);
+                printf("\naccount number = %d\n" user.accountnum);
+            }
+        }
+        fclose(fp2);
+ }
+}
+void deletefile
+
