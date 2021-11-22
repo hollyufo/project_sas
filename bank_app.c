8403bfafc8758b725414bcaf29974d91d2d285cb
hollyufo <chaibi1imran@gmail.com>
2021-11-22 15:23:32 +0100

    debugging 2

--- bank_app.c
+++ bank_app.c
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include<stdio.h>
 #include <stdlib.h>
 
@@ -37,9 +45,9 @@ void disp()
 void search()
 {
  FILE *fp2;
- int r, s, t;
+ char r, s, t;
  printf("\nEnter the Roll no you want to search  :");
- scanf("%d", &r);
+ scanf("%s", r);
  t = cincheck(r);
  if (t == 0)
   printf("ID %d is not available in the file\n",r);
