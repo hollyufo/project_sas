6e443b7f28e85e6b8f6b0a64435079ff34a18baa
hollyufo <chaibi1imran@gmail.com>
2021-11-22 08:46:04 +0100

    test

--- bank_app.c
+++ bank_app.c
@@ -57,3 +57,67 @@ void search(){
  }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+// MAIN PROGRAM
+void main()
+{
+ int c, emp;
+ do
+ {
+  printf("\n\t---Select your choice---------\n");
+  printf("\n\t1. INSERT\n\t2. DISPLAY\n\t3. SEARCH");
+  printf("\n\t4. DELETE\n\t5. UPDATE\n\t6. SORT");
+  printf("\n\t7. EXIT");
+  printf("\n\n------------------------------------------\n");
+  printf("\nEnter your choice:");
+  scanf("%d", &c);
+  printf("\n");
+  switch (c)
+  {
+  case 1:
+    insert();
+   break;
+  case 2:
+   emp = empty();
+   if (emp == 0)
+        printf("\nThe file is EMPTY\n");
+   else
+        disp();
+   break;
+  case 3:
+        search();
+   break;
+  case 4:
+        deletefile();
+   break;
+  case 5:
+        update();
+   break;
+  case 6:
+        emp = empty();
+        if (emp == 0)
+        printf("\n The file is EMPTY\n");
+   else
+        sort();
+   break;
+  case 7:
+        exit(1);
+   break;
+  default:
+        printf("\nYour choice is wrong\nPlease try again...\n");
+   break;
+
+  }
+ } while (c != 7);
+}
