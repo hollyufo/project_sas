11a925b19a60b3a09cd24ee0fa05bd4869c1c93f
hollyufo <chaibi1imran@gmail.com>
2021-11-22 09:46:49 +0100

    added main 2

--- bank_app.c
+++ bank_app.c
@@ -1,77 +1,42 @@
 #include<stdio.h>
-struct costumer
+#include <stdlib.h>
+
+struct coustumer{
+    char id[50];
+    char name[50];
+    int accountn;
+    float balance;
+}users;
+//function to insert info to the file 
+void insert()
 {
- char ID[20];
- char name[30];
- int accountnum;
- float balance;
-}user;
-// function to insert records to the file
-void insert(){
   FILE *fp;
   fp = fopen("Record", "a");
-  printf("Enter Id number : ");
-  scanf("%s", &user.ID);
+  printf("Enter CIN number:");
+  scanf("%s", &users.id[50]);
   printf("Enter the Name : ");
-  scanf("%s", &user.name);
-  printf("Enter account number : ");
-  scanf("%d", &user.accountnum)
+  scanf("%s", &users.name[20]);
+  printf("Enter the account number : ");
+  scanf("%d", &users.accountn);
   printf("Enter the balance : ");
-  scanf("%f", &user.balance);
-  fwrite(user, sizeof(user), 1, fp);
+  scanf("%f", &users.balance);
+  fwrite(&users, sizeof(users), 1, fp);
   fclose(fp);
 }
-//    FUNCTION TO DISPLAY RECORDS
+//function to display accounts
 void disp()
 {
     FILE *fp1;
     fp1 = fopen("Record", "r");
-    printf("\nid Number\tName\tbalance\tbalance\n\n");
-    while (fread(&stud, sizeof(stud), 1, fp1))
-    printf("  %s\t\t%s\t%d\t%.2f\n", student.id, user.name, user.accountnum, user.balance);
+    printf("\nid Number\tName\taccount Number\tbalance\n\n");
+    while (fread(&users, sizeof(users), 1, fp1))
+    printf("  %s\t\t%s\t%d\t%.2f\n", users.id, users.name, users.accountn, users.balance);
     fclose(fp1);
 }
-// function to search 
-void search(){
-    FILE *fp2;
-    int r, s, t;
-    printf("\nEnter the id you want to search : ");
-    scanf("%s", &r);
-    t = tid(r);
-    if (t == 0){
-        printf("the id %s is not available in the records.\n", r);
-    }
-    else {
-        fp2 = fopen("Record", "r");
-        while (fread(&user, sizeof(user), 1, fp2))
-        {
-            s = user.ID;
-            if (s == r){
-                printf("\nid  = %s", user.ID);
-                printf("\nName    = %s", user.name);
-                printf("\nbalnace    = %.2f\n", user.balance);
-                printf("\naccount number = %d\n" user.accountnum);
-            }
-        }
-        fclose(fp2);
- }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
 // MAIN PROGRAM
 void main()
 {
+
  int c, emp;
  do
  {
@@ -86,36 +51,32 @@ void main()
   switch (c)
   {
   case 1:
-    insert();
+   insert();
    break;
   case 2:
-   emp = empty();
-   if (emp == 0)
+    emp = empty();
+    if (emp == 0)
         printf("\nThe file is EMPTY\n");
    else
         disp();
    break;
   case 3:
-        search();
+   printf("\nThe file is EMPTY\n");
    break;
   case 4:
-        deletefile();
+   printf("\nThe file is EMPTY\n");
    break;
   case 5:
-        update();
+   printf("\nThe file is EMPTY\n");
    break;
   case 6:
-        emp = empty();
-        if (emp == 0)
-        printf("\n The file is EMPTY\n");
-   else
-        sort();
+    printf("\nThe file is EMPTY\n");
    break;
   case 7:
-        exit(1);
+   exit(1);
    break;
   default:
-        printf("\nYour choice is wrong\nPlease try again...\n");
+   printf("\nYour choice is wrong\nPlease try again...\n");
    break;
 
   }
